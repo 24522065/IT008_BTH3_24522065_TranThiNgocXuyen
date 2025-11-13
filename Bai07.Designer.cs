ea623fa912066e50eb888db94b1b383538f33f60
24522065 <24522065@gm.uit.edu.vn>
2025-11-13 20:42:31 +0700

    Create Bai07.Designer.cs

---
+++ Bai07.Designer.cs
@@ -0,0 +1,334 @@
+namespace Bai07
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            button6 = new Button();
+            button7 = new Button();
+            button8 = new Button();
+            button9 = new Button();
+            button10 = new Button();
+            button11 = new Button();
+            button12 = new Button();
+            button13 = new Button();
+            button14 = new Button();
+            button15 = new Button();
+            chooseButton = new Button();
+            cancelButton = new Button();
+            finishButton = new Button();
+            payment = new Label();
+            label3 = new Label();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.ForeColor = Color.Crimson;
+            label1.Location = new Point(12, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(217, 38);
+            label1.TabIndex = 0;
+            label1.Text = "MÀN ẢNH";
+            label1.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.White;
+            button1.Location = new Point(25, 70);
+            button1.Name = "button1";
+            button1.Size = new Size(71, 50);
+            button1.TabIndex = 1;
+            button1.Text = "1";
+            button1.UseVisualStyleBackColor = false;
+            button1.MouseClick += chooseNumber;
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.White;
+            button2.Location = new Point(102, 70);
+            button2.Name = "button2";
+            button2.Size = new Size(71, 50);
+            button2.TabIndex = 2;
+            button2.Text = "2";
+            button2.UseVisualStyleBackColor = false;
+            button2.MouseClick += chooseNumber;
+            // 
+            // button3
+            // 
+            button3.BackColor = Color.White;
+            button3.Location = new Point(179, 70);
+            button3.Name = "button3";
+            button3.Size = new Size(71, 50);
+            button3.TabIndex = 3;
+            button3.Text = "3";
+            button3.UseVisualStyleBackColor = false;
+            button3.MouseClick += chooseNumber;
+            // 
+            // button4
+            // 
+            button4.BackColor = Color.White;
+            button4.Location = new Point(256, 70);
+            button4.Name = "button4";
+            button4.Size = new Size(71, 50);
+            button4.TabIndex = 4;
+            button4.Text = "4";
+            button4.UseVisualStyleBackColor = false;
+            button4.MouseClick += chooseNumber;
+            // 
+            // button5
+            // 
+            button5.BackColor = Color.White;
+            button5.Location = new Point(333, 70);
+            button5.Name = "button5";
+            button5.Size = new Size(71, 50);
+            button5.TabIndex = 5;
+            button5.Text = "5";
+            button5.UseVisualStyleBackColor = false;
+            button5.MouseClick += chooseNumber;
+            // 
+            // button6
+            // 
+            button6.BackColor = Color.White;
+            button6.Location = new Point(25, 142);
+            button6.Name = "button6";
+            button6.Size = new Size(71, 50);
+            button6.TabIndex = 6;
+            button6.Text = "6";
+            button6.UseVisualStyleBackColor = false;
+            button6.MouseClick += chooseNumber;
+            // 
+            // button7
+            // 
+            button7.BackColor = Color.White;
+            button7.Location = new Point(102, 142);
+            button7.Name = "button7";
+            button7.Size = new Size(71, 50);
+            button7.TabIndex = 7;
+            button7.Text = "7";
+            button7.UseVisualStyleBackColor = false;
+            button7.MouseClick += chooseNumber;
+            // 
+            // button8
+            // 
+            button8.BackColor = Color.White;
+            button8.Location = new Point(179, 142);
+            button8.Name = "button8";
+            button8.Size = new Size(71, 50);
+            button8.TabIndex = 8;
+            button8.Text = "8";
+            button8.UseVisualStyleBackColor = false;
+            button8.MouseClick += chooseNumber;
+            // 
+            // button9
+            // 
+            button9.BackColor = Color.White;
+            button9.Location = new Point(256, 142);
+            button9.Name = "button9";
+            button9.Size = new Size(71, 50);
+            button9.TabIndex = 9;
+            button9.Text = "9";
+            button9.UseVisualStyleBackColor = false;
+            button9.MouseClick += chooseNumber;
+            // 
+            // button10
+            // 
+            button10.BackColor = Color.White;
+            button10.Location = new Point(333, 142);
+            button10.Name = "button10";
+            button10.Size = new Size(71, 50);
+            button10.TabIndex = 10;
+            button10.Text = "10";
+            button10.UseVisualStyleBackColor = false;
+            button10.MouseClick += chooseNumber;
+            // 
+            // button11
+            // 
+            button11.BackColor = Color.White;
+            button11.Location = new Point(25, 208);
+            button11.Name = "button11";
+            button11.Size = new Size(71, 50);
+            button11.TabIndex = 11;
+            button11.Text = "11";
+            button11.UseVisualStyleBackColor = false;
+            button11.MouseClick += chooseNumber;
+            // 
+            // button12
+            // 
+            button12.BackColor = Color.White;
+            button12.Location = new Point(102, 208);
+            button12.Name = "button12";
+            button12.Size = new Size(71, 50);
+            button12.TabIndex = 12;
+            button12.Text = "12";
+            button12.UseVisualStyleBackColor = false;
+            button12.MouseClick += chooseNumber;
+            // 
+            // button13
+            // 
+            button13.BackColor = Color.White;
+            button13.Location = new Point(179, 208);
+            button13.Name = "button13";
+            button13.Size = new Size(71, 50);
+            button13.TabIndex = 13;
+            button13.Text = "13";
+            button13.UseVisualStyleBackColor = false;
+            button13.MouseClick += chooseNumber;
+            // 
+            // button14
+            // 
+            button14.BackColor = Color.White;
+            button14.Location = new Point(256, 208);
+            button14.Name = "button14";
+            button14.Size = new Size(71, 50);
+            button14.TabIndex = 14;
+            button14.Text = "14";
+            button14.UseVisualStyleBackColor = false;
+            button14.MouseClick += chooseNumber;
+            // 
+            // button15
+            // 
+            button15.BackColor = Color.White;
+            button15.Location = new Point(333, 208);
+            button15.Name = "button15";
+            button15.Size = new Size(71, 50);
+            button15.TabIndex = 15;
+            button15.Text = "15";
+            button15.UseVisualStyleBackColor = false;
+            button15.MouseClick += chooseNumber;
+            // 
+            // chooseButton
+            // 
+            chooseButton.Location = new Point(25, 369);
+            chooseButton.Name = "chooseButton";
+            chooseButton.Size = new Size(112, 34);
+            chooseButton.TabIndex = 16;
+            chooseButton.Text = "Chọn";
+            chooseButton.UseVisualStyleBackColor = true;
+            chooseButton.Click += chooseButton_Click;
+            // 
+            // cancelButton
+            // 
+            cancelButton.Location = new Point(138, 369);
+            cancelButton.Name = "cancelButton";
+            cancelButton.Size = new Size(112, 34);
+            cancelButton.TabIndex = 17;
+            cancelButton.Text = "Hủy bỏ";
+            cancelButton.UseVisualStyleBackColor = true;
+            cancelButton.Click += cancelButton_Click;
+            // 
+            // finishButton
+            // 
+            finishButton.Location = new Point(267, 369);
+            finishButton.Name = "finishButton";
+            finishButton.Size = new Size(112, 34);
+            finishButton.TabIndex = 18;
+            finishButton.Text = "Kết thúc";
+            finishButton.UseVisualStyleBackColor = true;
+            finishButton.Click += finishButton_Click;
+            // 
+            // payment
+            // 
+            payment.BackColor = SystemColors.ControlDarkDark;
+            payment.Font = new Font("Segoe UI", 16F);
+            payment.Location = new Point(197, 291);
+            payment.Name = "payment";
+            payment.Size = new Size(207, 38);
+            payment.TabIndex = 19;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(65, 304);
+            label3.Name = "label3";
+            label3.Size = new Size(98, 25);
+            label3.TabIndex = 20;
+            label3.Text = "Thành tiền:";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(label3);
+            Controls.Add(payment);
+            Controls.Add(finishButton);
+            Controls.Add(cancelButton);
+            Controls.Add(chooseButton);
+            Controls.Add(button15);
+            Controls.Add(button14);
+            Controls.Add(button13);
+            Controls.Add(button12);
+            Controls.Add(button11);
+            Controls.Add(button10);
+            Controls.Add(button9);
+            Controls.Add(button8);
+            Controls.Add(button7);
+            Controls.Add(button6);
+            Controls.Add(button5);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Button button5;
+        private Button button6;
+        private Button button7;
+        private Button button8;
+        private Button button9;
+        private Button button10;
+        private Button button11;
+        private Button button12;
+        private Button button13;
+        private Button button14;
+        private Button button15;
+        private Button chooseButton;
+        private Button cancelButton;
+        private Button finishButton;
+        private Label payment;
+        private Label label3;
+    }
+}
